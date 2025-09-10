eb8d8e1c5f4c25578440010d908c819d49e08878
JaylordFPH <JaySoguilonIS@gmail.com>
2025-09-11 02:02:54 +0800

    simple solution

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let dataParam = req.params.date
+  let date;
+
+  if(dataParam.includes("-")) {
+    date = new Date(dataParam)
+  } else {
+    date = new Date(parseInt(dataParam))
+  }
+
+  res.json({unix: `${date.getDate()}`, utc: `${date.toUTCString()}`});
 });
 
 
